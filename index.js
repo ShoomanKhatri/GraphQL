5e4be90814cb7fcf7f35f8a1d2628818559b9e22
Laxmi-KC <159879082+Laxmi-KC@users.noreply.github.com>
2024-07-28 09:50:09 +0000

    upated

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// Define schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Define resolvers
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+// Create Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Start the server
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
